4b16a47e112af75cc5f56b1c47666247aff7bd9c
juan feas <juan.feas@bue.edu.ar>
2023-02-01 21:18:05 -0300

    termine ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,15 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//creo ariable para guardar el nombre
+	let nombreIngresado;
+
+	//asigno el nombre del usuario a la variable
+	nombreIngresado = prompt("ingrese su nombre");
+
+//muestro el nombre ingresado
+	alert(nombreIngresado);
 }
 
+//operador de asignacion =
+//const NOMBRE = "juan"
